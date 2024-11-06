62aeb0a1df2c17e61d342aa22df064f8c56a2feb
andrzej <andrzej.brusnicyn@gmail.com>
2024-11-06 15:16:49 +0300

    fix delete user

--- router/web.php
+++ router/web.php
@@ -7,6 +7,10 @@ use Controllers\UserController;
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
+if ($requestMethod === 'POST' && isset($_POST['_method'])) {
+    $requestMethod = strtoupper($_POST['_method']);
+}
+
 $controller = new UserController();
 
 switch ($requestUri) {
@@ -43,7 +47,7 @@ switch ($requestUri) {
         }
         elseif (preg_match('/^\/users\/(\d+)\/edit$/', $requestUri, $matches)) {
             $userId = (int)$matches[1];
-            if ($requestMethod === 'POST') {
+            if ($requestMethod === 'PUT') {
                 $controller->update($userId);
             }
             else {
